bf56e51c7f34fa8b99bb5681e45a7206ac1f4e5f
Ionel Condor <ionel-condor@yahoo.com>
2015-02-21 12:31:49 +0200

    determine trials

--- src/fasttrackit/lottogame/LottoV1.java
+++ src/fasttrackit/lottogame/LottoV1.java
@@ -9,6 +9,8 @@ public class LottoV1 {
 
     public static void main(String[] args) {
 
+        int[] sixGeneratedNumbers = generateLottoMachine();
+
         //  read users' numbers and store them
         //  prepare the lotto machine and init it
         //  generate the lotto numbers
@@ -23,27 +25,91 @@ public class LottoV1 {
         // comment
         // ...
 
-        // 1 read
-        int[] myNumbers = new int[MAX_EXTRACTION_NUMBERS];
-        Random myNumbersMachine = new Random();
-       
-        for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++) {
-            int nr = myNumbersMachine.nextInt(MAXVALUE) + 1;
-            //check if nr is not already in the array, if it is generate a new one
-            for (int j = 0; j < i; j++)
-                if (myNumbers[j] == nr) nr = myNumbersMachine.nextInt(MAXVALUE) + 1;
-            myNumbers[i] = nr;
+        int tentatives = 0;
+        int howManyWons = 0;
+        int[] wonNumbers = new int[MAX_EXTRACTION_NUMBERS];
+
+
+        do {
+
+            // 1 read
+            int[] myNumbers = new int[MAX_EXTRACTION_NUMBERS];
+            Random myNumbersMachine = new Random();
+
+            for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++) {
+                int nr = myNumbersMachine.nextInt(MAXVALUE) + 1;
+                //check if nr is not already in the array, if it is generate a new one
+                for (int j = 0; j < i; j++)
+                    if (myNumbers[j] == nr) nr = myNumbersMachine.nextInt(MAXVALUE) + 1;
+                myNumbers[i] = nr;
+            }
+            // here are my numbers
+            System.out.println("here are my numbers:");
+            for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
+                System.out.print(myNumbers[i] + " |");
+
+            System.out.println("");
+
+
+            //compare and tell if won
+            howManyWons = 0;
+
+
+            for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
+                for (int j = 0; j < MAX_EXTRACTION_NUMBERS; j++) {
+
+                    if (myNumbers[i] == sixGeneratedNumbers[j]) {
+                        howManyWons++;
+                        wonNumbers[howManyWons - 1] = sixGeneratedNumbers[j];
+                    }
+                }
+
+            switch (howManyWons) {
+
+                case 3: // that is 4 numbers
+                    System.out.println("congrat, you won at 3rd category");
+                    break;
+                case 4: // that is 5 numbers
+                    System.out.println("congrat, you won at 2rd category");
+                    break;
+                case 5: // that is 6 numbers
+                    System.out.println("WOW, you won at 1st category");
+                    break;
+                default:
+                    System.out.println("you are a looser, but keep trying, you guessed " + howManyWons + " numbers");
+                    break;
+            }
+
+            tentatives++;
+        } while (howManyWons <= 3);
+
+
+        if (howManyWons >= 4) {
+
+            //extraction
+            // 3 print the extraction
+            System.out.println("here is the extraction today:");
+            for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
+                System.out.print(sixGeneratedNumbers[i] + " |");
+
+            System.out.println("");
+
+
+            System.out.println("tentatives:"+tentatives);
+            System.out.println("here is what you won: ");
+            for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
+                if (wonNumbers[i] != 0)
+                    System.out.print(wonNumbers[i] + " |");
+
         }
-        // here are my numbers
-        System.out.println("here are my numbers:");
-        for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
-            System.out.print(myNumbers[i] + " |");
+        else
+        {
+            System.out.println("sorry");
+        }
 
-        System.out.println("");
+    }
 
-
-
-
+    private static int[] generateLottoMachine() {
         // 2 prepare the lotto machine and init it
         Random lottoMachine = new Random();
         lottoMachine.setSeed(System.currentTimeMillis());
@@ -61,12 +127,7 @@ public class LottoV1 {
 
             sixGeneratedNumbers[i] = nr;
 
-            // wait , not necessary anyway
-            try {
-                Thread.currentThread().sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
 
         }
 
@@ -76,40 +137,6 @@ public class LottoV1 {
             System.out.print(sixGeneratedNumbers[i] + " |");
 
         System.out.println("");
-
-        //compare and tell if won
-        int howManyWons = 0;
-        int[] wonNumbers = new int[MAX_EXTRACTION_NUMBERS];
-
-        for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
-            for (int j = 0; j < MAX_EXTRACTION_NUMBERS; j++) {
-
-                if (myNumbers[i] == sixGeneratedNumbers[j]) {
-                    howManyWons++;
-                    wonNumbers[howManyWons - 1] = sixGeneratedNumbers[j];
-                }
-            }
-
-        switch (howManyWons) {
-
-            case 3: // that is 4 numbers
-                System.out.println("congrat, you won at 3rd category");
-                break;
-            case 4: // that is 5 numbers
-                System.out.println("congrat, you won at 2rd category");
-                break;
-            case 5: // that is 6 numbers
-                System.out.println("WOW, you won at 1st category");
-                break;
-            default:
-                System.out.println("you are a looser, but keep trying, you guessed " + howManyWons +" numbers");
-                break;
-        }
-
-        System.out.println("here is what you won: ");
-        for (int i = 0; i < MAX_EXTRACTION_NUMBERS; i++)
-            if (wonNumbers[i] != 0)
-                System.out.print(wonNumbers[i] + " |");
-
+        return sixGeneratedNumbers;
     }
 }
