29b99693f4690fb89d5b4e9cb48bd2de889aa6e1
Ionel Condor <ionel-condor@yahoo.com>
2015-11-21 09:54:26 +0200

    demo how to use a library

---
+++ src/fasttrackit/lottogame/Ionel.java
@@ -0,0 +1,19 @@
+package fasttrackit.lottogame;
+
+import java.util.LinkedList;
+import java.util.List;
+import ionel.demolib.*;
+
+/**
+ * Created by condor on 21/11/15.
+ */
+public class Ionel  {
+    public static void main(String[] argv) {
+         List l1 = new DenissaList();
+        l1.add("ana");
+        l1.add("are");
+        l1.add("mere");
+        l1.remove(0);
+
+    }
+}
